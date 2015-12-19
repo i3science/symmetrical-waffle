e190f7129cd1702472027e9ce3266cdadf286a13
Derek <derekabois@gmail.com>
2015-12-18 22:29:50 -0500

    minor

--- src/js/client/components/app.js
+++ src/js/client/components/app.js
@@ -2,7 +2,7 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import { Router } from 'react-router';
 import Routes from './routes.jsx';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
-// <Router history={createBrowserHistory()}>
+
 export default () => {
 	return (
 		<Router history={createBrowserHistory()}>
