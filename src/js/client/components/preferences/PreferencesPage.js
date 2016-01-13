817e77fb92ad912a609cb959af4a9705bd614722
Thomas Wilson <thomas@thomaspwilson.com>
2016-01-13 12:30:57 -0500

    ES6 compliant prefs page and template components. Changed influencer age to number

--- src/js/client/components/preferences/PreferencesPage.js
+++ src/js/client/components/preferences/PreferencesPage.js
@@ -1,20 +1,28 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { Link } from 'react-router';
 
-export default (props) => {
-    return (
-        <div className="row">
-            <div className="col s3">
-                <h3>Preferences</h3>
-                <ul>
-                    <Link to="/prefs/accounts/me">My Account</Link>
-                    <Link to="/prefs/influencers">Influencers</Link>
-                    <Link to="/prefs/clients">Clients</Link>
-                </ul>
+class PreferencesPage extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col s3">
+                    <h3>Preferences</h3>
+                    <ul>
+                        <Link to="/prefs/accounts/me">My Account</Link>
+                        <Link to="/prefs/influencers">Influencers</Link>
+                        <Link to="/prefs/clients">Clients</Link>
+                    </ul>
+                </div>
+                <div className="col s9">
+                    {this.props.children}
+                </div>
             </div>
-            <div className="col s9">
-                {props.children}
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
+
+export default PreferencesPage;
--- src/js/client/components/template.js
+++ src/js/client/components/template.js
@@ -1,15 +1,17 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import Header from './header';
 
-export default (props) => {
-    return (
-        <div>
-            <Header
-                path={props.location.pathname}
-            />
-            <div className="container section">
-                {props.children}
+class Template extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header
+                    props={this.props} />
+                <div className="container section">
+                    {this.props.children}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
+export default Template;
