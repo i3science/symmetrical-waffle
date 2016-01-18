fe1ab5c5ef8290c68a97ffa38d076d7cf2fafef8
Thomas Wilson <thomas@thomaspwilson.com>
2016-01-18 09:02:41 -0500

    Implement isomorphic rendering.

--- src/js/client/utils/Object.js
+++ src/js/client/utils/Object.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
-Object.prefixKeys = function(prefix) {
-    let tmp = _.extend({}, this);
+Object.prefixKeys = function(arr, prefix) {
+    let tmp = _.extend({}, arr);
     Object.keys(tmp).map(function(k){
         let v = tmp[k];
         delete tmp[k];
