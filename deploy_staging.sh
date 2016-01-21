e9395720faf6fe1fe6a17782bbd705285a55aa3f
Thomas Wilson <thomas@thomaspwilson.com>
2016-01-20 23:20:04 -0500

    Continuing work on CircleCI deployment

--- deploy_staging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TARGET=54.173.3.126
-
-tar -czf dist.tgz config/ dist/ seed/ src/mail src/public gulpfile.js server.js \
- && rsync -avz -e "ssh -p 9731" dist.tgz circleci@${TARGET}:/home/circleci \
- && ssh circleci@${TARGET} -p 9731 "sudo /home/circleci/deploy.sh"
