75d8a585f3d146f825aa091df25bfd96d644f56c
Thomas Wilson <thomas@thomaspwilson.com>
2016-02-01 09:09:09 -0500

    Fix staging issues

---
+++ config/env/staging.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  db: {
+    uri: (process.env.MONGO_BASE || 'mongodb://localhost/') + 'smp',
+    options: {
+      user: '',
+      pass: ''
+    }
+  },
+  mail: {
+    disable: true,
+    transport: null
+  },
+  log: {
+    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+    format: 'dev',
+    // Stream defaults to process.stdout
+    // Uncomment to enable logging to a log on the file system
+    options: {
+      //stream: 'access.log'
+    }
+  },
+  app: {
+    title: 'Social Marketplace - Development Edition'
+  }
+};
