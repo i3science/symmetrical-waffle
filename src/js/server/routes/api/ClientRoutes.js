f61b8bd7813a3010f61ab2814aa42cad6ba38bd8
Thomas Wilson <thomas@thomaspwilson.com>
2016-02-21 11:21:40 -0500

    Lock down the client list to return only those that an influencer or rep are related to

--- src/js/server/routes/api/ClientRoutes.js
+++ src/js/server/routes/api/ClientRoutes.js
@@ -3,7 +3,7 @@ import authenticationController from '../../controllers/AuthenticationController
 
 module.exports = function(app) {
     app.route('/api/clients')
-        .get(authenticationController.hasRole(['organizer','client']), clientController.list)
+        .get(authenticationController.isLoggedIn, clientController.list)
         .post(authenticationController.hasRole(['organizer','client']), clientController.create);
     app.route('/api/clients/:clientId')
         .get(authenticationController.hasRole(['organizer','client']), clientController.read)
