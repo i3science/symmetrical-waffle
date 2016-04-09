8d86bcc58c668adead05e0a3e3b0f911bd808b72
Thomas Wilson <thomas@thomaspwilson.com>
2016-04-09 17:21:16 -0400

    Add ability to upload files to tasks

--- src/js/server/controllers/TaskController.js
+++ src/js/server/controllers/TaskController.js
@@ -1,6 +1,47 @@
+import Q from 'q';
+import fs from 'fs';
+import path from 'path';
+import mkdirp from 'mkdirp';
+import config from '../../../../config/config';
 import taskService from '../services/TaskService.js';
 import base_controller from './base_controller';
 import ErrorUtils from '../utils/ErrorUtils';
+
+let copy = (oldPath, newPath) => {
+    let deferred = Q.defer();
+    let readStream = fs.createReadStream(oldPath);
+    let writeStream = fs.createWriteStream(newPath);
+
+    readStream.on('error', (err) => {
+        deferred.reject(err);
+    });
+    writeStream.on('error', (err) => {
+        deferred.reject(err);
+    });
+    readStream.on('close', () => {
+        fs.unlink(oldPath, () => {
+            deferred.resolve(true);
+        });
+    });
+    return deferred.promise;
+};
+
+let move = (oldPath, newPath) => {
+
+    let deferred = Q.defer();
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+            if (err.code === 'EXDEV') {
+                copy(oldPath, newPath);
+            } else {
+                deferred.reject(err);
+            }
+            return;
+        }
+        deferred.resolve(true);
+    });
+
+};
 
 export default base_controller(taskService, 'task', {
     list(req, res) {
@@ -13,11 +54,53 @@ export default base_controller(taskService, 'task', {
     },
     create(req, res) {
         let obj = this.sanitize(req.body);
+        if (req.file) {
+            obj.filename = req.file.originalname;
+            obj.mime = req.file.mimetype || 'application/octet-stream';
+        }
+
+        let task = null;
+        let fullpath = null;
         return taskService
             .create(req.element, obj)
-            .spread((task) => {
+            .spread((_task) => {
+                task = _task;
+                if (!req.file) {
+                    return true;
+                }
+
+                let path = task._id.toString().match(/.{1,3}/g).join('/');
+                fullpath = config.filesDir + '/' + path;
+                let deferred = Q.defer();
+                mkdirp(fullpath, function(err){
+                    if (err) {
+                        deferred.reject(err);
+                    } else {
+                        deferred.resolve(true);
+                    }
+                });
+                return deferred.promise
+                    .then(() => {
+                        move(req.file.path, fullpath + '/' + task._id.toString());
+                    });
+            })
+            .then(() => {
                 return res.status(201).send({ id: task._id });
             })
             .fail(ErrorUtils.failureHandler(req, res));
+    },
+    file(req, res) {
+        let _path = req.task._id.toString().match(/.{1,3}/g).join('/');
+        let fullpath = path.resolve(config.filesDir, _path);
+        fullpath = path.resolve(fullpath, req.task._id.toString());
+        fs.access(fullpath, fs.R_OK, (err) => {
+            if (err) {
+                return res.status(404).send();
+            }
+            res.setHeader('Content-disposition', 'attachment; filename='+req.task.filename);
+            res.setHeader('Content-Type', req.task.mime || 'application/octet-stream');
+            let filestream = fs.createReadStream(fullpath);
+            filestream.pipe(res);
+        });
     }
 });
--- src/js/server/routes/api/TaskRouter.js
+++ src/js/server/routes/api/TaskRouter.js
@@ -1,15 +1,18 @@
 import express from 'express';
+import config from '../../../../../config/config';
 import authenticationController from '../../controllers/AuthenticationController';
 import taskController from '../../controllers/TaskController';
 
 let router = express.Router();
 router.route('/')
     .get(authenticationController.isLoggedIn, taskController.list)
-    .post(authenticationController.isLoggedIn, taskController.create);
+    .post(authenticationController.isLoggedIn, config.uploader.single('file'), taskController.create);
 router.route('/:taskId')
     .get(authenticationController.isLoggedIn, taskController.read)
-    .put(authenticationController.isLoggedIn, taskController.update)
+    .put(authenticationController.isLoggedIn, config.uploader.single('file'), taskController.update)
     .delete(authenticationController.hasRole(['organizer']), taskController.delete);
+router.route('/:taskId/file')
+	.get(authenticationController.isLoggedIn, taskController.file);
 
 router.param('taskId', taskController.findById);
 
