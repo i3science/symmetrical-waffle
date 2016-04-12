67152d0b8a3e0aa1640925ba0d27cfdb79e8242f
Derek <derekabois@gmail.com>
2016-04-12 15:48:05 -0400

    Add Personal and Audience pages to Influencer workflow

--- src/js/client/components/preferences/audience/AudienceEditPage.js
+++ src/js/client/components/preferences/audience/AudienceEditPage.js
@@ -1,163 +1,109 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
-import InputText from '../../common/input/inputtext';
-import authenticationStore from '../../../stores/AuthenticationStore';
+import React from 'react';
 import Actions from '../../../actions/UiActions';
+import InfluencerStore from '../../../stores/InfluencerStore';
+import InfluencerAudienceForm from './InfluencerAudienceForm';
+import AuthenticationStore from '../../../stores/AuthenticationStore';
 
 class AudienceEditPage extends React.Component {
     constructor() {
         super();
         this.state = {
-            user: authenticationStore.getCurrentUser()
+            influencer: AuthenticationStore.getCurrentUser()
         };
+        this._onChange = this._onChange.bind(this);
         this._handleChange = this._handleChange.bind(this);
         this._onSubmit = this._onSubmit.bind(this);
+        this._onSend = this._onSend.bind(this);
+        this._cancel = this._cancel.bind(this);
+        this._expand = this._expand.bind(this);
     }
+
+    componentWillMount() {
+        InfluencerStore.addChangeListener(this._onChange);
+    }
+
+    componentWillUnmount() {
+        InfluencerStore.removeChangeListener(this._onChange);
+    }
+
+    _onChange() {
+        let currentInfluencer = InfluencerStore.getCurrentInfluencer();
+        if (currentInfluencer) {
+            this.setState({influencer: currentInfluencer});
+        }
+    }
+
     _handleChange(event) {
         let value = event.target.value;
         let id = event.target.id;
-
+        if (id.indexOf('_range_') !== -1) {
+            id = id.split('_')[0];
+        }
         if (event.target.type === 'number') {
             value = Number(value);
         }
         if (event.target.type === 'checkbox') {
-            value = event.target.checked;
-        }
-        if (event.target.type === 'radio') {
-            id = event.target.name;
-            value = event.target.id;
-        }
-        if (!event.target.dataset.parent) {
-            this.state.user[id] = value;
+            let isIn = this.state.influencer[event.target.dataset.parent].indexOf(event.target.name);
+            if ((isIn === -1) && value) {
+                this.state.influencer[event.target.dataset.parent].push(event.target.name);
+            } else {
+                this.state.influencer[event.target.dataset.parent].splice(isIn, 1);
+            }
         } else {
-            this.state.user[event.target.dataset.parent][id] = value;
+            if (!event.target.dataset.parent) {
+                this.state.influencer[id] = value;
+            } else {
+                if (id.indexOf('_')) {
+                    let deeper = id.split('_').pop();
+                    id = id.split('_')[0];
+                    this.state.influencer[event.target.dataset.parent][id][deeper] = value;
+                } else {
+                    this.state.influencer[event.target.dataset.parent][id] = value;
+                }
+            }
         }
-        this.setState({user: this.state.user});
+        this.setState({influencer: this.state.influencer});
     }
+    _cancel() {
+        this.setState({influencer: {}});
+        this.props.history.goBack();
+    }
+
     _onSubmit(event) {
         event.preventDefault();
-
-        let file = $(event.target).find('input[type="file"]')[0].files[0];
-        if (!file) {
-            // TODO Error no file given
-            return;
+        if (!this.state.influencer._id) {
+            Actions.createInfluencer(this.state.influencer);
+        } else {
+            Actions.updateInfluencer(this.state.influencer);
         }
-
-        let self = this;
-        let reader = new FileReader();
-        reader.addEventListener('load', function(){
-            let user = this.state.user;
-            user.image = reader.result;
-            Actions.updateUser(self.state.user);
-            this.setState({ user: user });
-        }.bind(this), false);
-        reader.readAsDataURL(file);
     }
-    render() {
 
-        let user_image = (<img className="circle responsive-img" src="/assets/images/default.jpg"/>);
-        if (this.state.user.image) {
-            user_image = (<div className="circle responsive-img" style={{
-                background: 'url('+this.state.user.image+') no-repeat rgba(0,0,0,0.3) 50%',
-                backgroundSize: 'cover',
-                width: '100%',
-                padding: '50%' }}></div>);
+    _onSend() {
+        Actions.sendInfluencer(this.state.influencer);
+    }
+
+    _expand(event) {
+        var advanced = document.getElementById('advanced-collapse');
+        if (event.target.checked) {
+            advanced.style.maxHeight = '1500px';
+        } else {
+            advanced.style.maxHeight = '0';
         }
+    }
 
+    render() {
         return (
-            <div className="card-panel z-depth-4">
-                <h4 className="center-align">Edit Your Account</h4><br />
-                <form action={'/users/'+this.state.user._id} onSubmit={this._onSubmit} method="put" encType="multipart/form-data">
-                    <div className="row">
-                        <div className="col s8" style={{float: 'none', margin: '0 auto'}}>
-                            <div className="row">
-                                <div className="col s2">
-                                    {user_image}
-                                </div>
-                                <div className="col s10">
-                                    <div className="file-field input-field">
-                                        <div className="btn">
-                                            <span>File</span>
-                                            <input type="file" name="file" id="file"/>
-                                        </div>
-                                        <div className="file-path-wrapper">
-                                            <input className="file-path validate" type="text" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col s6">
-                                    <InputText
-                                        id="first"
-                                        label="First Name"
-                                        val={this.state.user.name.first}
-                                        parent="name"
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                                <div className="col s6">
-                                    <InputText
-                                        id="lastname"
-                                        label="Last Name"
-                                        val={this.state.user.name.last}
-                                        parent="name"
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col s6">
-                                    <InputText
-                                        id="email"
-                                        label="Email Address"
-                                        val={this.state.user.email}
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                                <div className="col s6">
-                                    <InputText
-                                        id="timezone"
-                                        label="Time Zone"
-                                        val={this.state.user.timezone}
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col s6">
-                                    <InputText
-                                        type="password"
-                                        id="password"
-                                        label="Password"
-                                        val={this.state.user.password}
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                                <div className="col s6">
-                                    <InputText
-                                        type="password"
-                                        id="confirm"
-                                        label="Confirm Password"
-                                        val={this.state.user.confirm}
-                                        onChange={this._handleChange}
-                                        active
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                        <div className="col 12" style={{float: 'none'}}>
-                            <button type="submit" className="teal waves-effect waves-light btn-large right">Save Changes</button>
-                        </div>
-                    </div>
-                </form>
+            <div>
+                <div className="card-panel z-depth-4">
+                    <h4 className="center-align" style={{marginBottom: '30px'}}>Edit Your Audience Details</h4>
+                    <InfluencerAudienceForm
+                        influencer={this.state.influencer}
+                        onChange={this._handleChange}
+                        onSubmit={this._onSubmit}
+                        onSend={this._onSend}
+                        cancel={this._cancel}
+                    />
+                </div>
             </div>
         );
     }
